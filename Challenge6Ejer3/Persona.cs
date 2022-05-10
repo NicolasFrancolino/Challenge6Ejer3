a55387b55220709b03596110d709597343af6ccb
NicolasFrancolino <87394098+NicolasFrancolino@users.noreply.github.com>
2022-05-10 13:45:27 -0300

    Agregar archivos de proyecto.

---
+++ Challenge6Ejer3/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge6Ejer3
+{
+    partial class Persona
+    {
+        public string Nombre { get; set; }
+        public DateTime FechaNac { get; set; }
+
+        public Persona(string nombre, DateTime fecha)
+        {
+            Nombre = nombre;
+            FechaNac = fecha;
+        }
+    }
+}
---
+++ Challenge6Ejer3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge6Ejer3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese fecha de nacimiento en el siguiente formato:");
+            Console.WriteLine("Dia/Mes/Año");
+            string fechaAux = Console.ReadLine();
+            DateTime fecha = DateTime.ParseExact(fechaAux, "dd/MM/yyyy", null);
+            Persona p = new Persona(nombre, fecha);
+            p.Edad(); 
+            Console.ReadKey();  
+        }
+    }
+}
